14cddcdfeee5e3ac4c67c7f10e5ae383f19d7f0c
itsachaljain <achaljain785@gmail.com>
2020-10-14 01:36:35 +0530

    Messages Fixed

--- src/Components/Routing.js
+++ src/Components/Routing.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Chatbox from "./ChatboxComponent";
 import Login from "./LoginComponent";
 
